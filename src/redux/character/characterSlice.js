3b8d4dcef31a743e38c8ae51a1e38171af36a998
rabia ölker <rabia.olker@gmail.com>
2022-04-21 00:04:24 +0300

    Data extraction was done with axios.

--- src/redux/character/characterSlice.js
+++ src/redux/character/characterSlice.js
@@ -1,8 +1,10 @@
 import {createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const char_limit=12
+
 export const fetchCharacters=createAsyncThunk("characters/getCharacters", async ()=>{
-    const res= await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}`)
+    const res= await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=${char_limit}&offset=0`)
     return res.data
 }) 
 
@@ -10,11 +12,23 @@ export const charactersSlice=createSlice({
     name:"character",
     initialState:{
         items:[],
+        isLoading:false,
+        error:"",
     },
     reducers:{},
     extraReducers:{
+        [fetchCharacters.pending]:(state)=>{
+            state.isLoading=true
+        },
         [fetchCharacters.fulfilled]:(state,action)=>{
-            state.items.push(action.payload)
+            state.items=action.payload
+            state.isLoading=false
+        },
+       
+        [fetchCharacters.rejected]:(state,action)=>{
+            state.isLoading=false;
+            state.error=action.error.message;
+
         }
     }
 })
