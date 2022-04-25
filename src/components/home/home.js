4c554ee35cdc4eb94206f0936a541bf4a8fb9016
rabia ölker <rabia.olker@gmail.com>
2022-04-25 18:53:24 +0300

    load more button add

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,32 +1,41 @@
 import {useEffect} from 'react'
 import {useSelector, useDispatch} from "react-redux"
-import { fetchCharacters } from '../../redux/character/characterSlice'
-import {Container} from "react-bootstrap"
+import { fetchCharacters, pageIncrement } from '../../redux/character/characterSlice'
+import {Container, Button} from "react-bootstrap"
 import Masonry from 'react-masonry-css'
 import { Link } from "react-router-dom";
+
 
 function Home() {
   const items=useSelector(state=>state.character.items)
   const isLoading=useSelector(state=>state.character.isLoading)
   const error=useSelector(state=>state.character.error)
-
-
+  const page=useSelector(state=>state.character.page)
   const dispatch=useDispatch();
+
   useEffect(() => {
    dispatch(fetchCharacters())
-  }, [dispatch])
+  }, [dispatch])  
 
+  const handleClick=async()=>{
+    await dispatch(pageIncrement())
+    await dispatch(fetchCharacters(page))
+  }
+
+/** Display of loading and error states on the page -start-*/
   if(isLoading){
     return(
       <div className="spinner-border text-light" role="status" style={{padding:"5%", marginLeft:"45%"}}>
-        <span className="visually-hidden">Loading...</span>
+        <span className="visually-hidden ">Loading...</span>
       </div>
     )} 
 
     if(error){
-      return <div>Error:{error}</div>
+      return <div className='text-light ms-5'>Error:{error}</div>
     }
-   
+   /** Display of loading and error states on the page -end-*/
+
+
   return (
       <Container >
          <Masonry
@@ -45,7 +54,11 @@ function Home() {
           ))
         }
       </Masonry>
-
+        {
+          items.length!==62 && (
+            <Button variant='light' className='char-btn' onClick={handleClick} >Load More ({page-2})</Button>
+          )
+        }
       </Container>
        
   )
