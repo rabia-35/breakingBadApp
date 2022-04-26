8fb166d609579941612b77015fcd2d9f56cd969e
rabia ölker <rabia.olker@gmail.com>
2022-04-26 16:13:33 +0300

    hasNextPage added.

--- src/redux/character/characterSlice.js
+++ src/redux/character/characterSlice.js
@@ -5,7 +5,7 @@ const char_limit=12
 
 /** Data extraction with axios via API -start- */
 export const fetchCharacters=createAsyncThunk("characters/getCharacters", async (page)=>{
-    const res= await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=${page ?  char_limit * page : 12}&offset=0`)
+    const res= await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=${char_limit}&offset=${page ? page * char_limit : 0}`)
     return res.data
 }) 
 /** Data extraction with axios via API -end- */
@@ -17,7 +17,8 @@ export const charactersSlice=createSlice({
         items:[],
         isLoading:false,
         error:"",
-        page:2,
+        page:1,
+        hasNextPage:true,
     },
     reducers:{
         pageIncrement:(state)=>{
@@ -29,8 +30,11 @@ export const charactersSlice=createSlice({
             state.isLoading=true
         },
         [fetchCharacters.fulfilled]:(state,action)=>{
-            state.items=action.payload
+            state.items=[...state.items,...action.payload]
             state.isLoading=false
+            if(action.payload.length< 12){
+                state.hasNextPage=false
+            }
         },
        
         [fetchCharacters.rejected]:(state,action)=>{
