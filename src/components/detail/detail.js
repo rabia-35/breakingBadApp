fe93595a136d870c6508a5b44197c82ab096f5ba
rabia ölker <rabia.olker@gmail.com>
2022-04-26 16:09:03 +0300

    the detail page  createde with useparams and Link

--- src/components/detail/detail.js
+++ src/components/detail/detail.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
+import {useSelector} from "react-redux"
+import {Container, Row, Col} from "react-bootstrap"
 
 function Detail() {
+  let { char_id } = useParams(); // Getting id from URL with useParams
+  char_id=char_id.match(/[0-9]+/gi).join("") //Converting the drawn id to number
+  
+  const items=useSelector(state=>state.character.items)
+  const item=items.filter(item=> item.char_id===Number(char_id)) //Finding the character that matches the id from the URL
+ 
   return (
-    <div>Detail</div>
+    <Container>
+     <Row className="justify-content-md-center">
+       <Col  >
+          <img  className="detail-img" alt='characterImage' src={item[0].img} />
+       </Col>
+       <Col >
+         <h5>{item[0].name} </h5>
+         <p > Birthday :  {item[0].birthday}</p>
+         <p> Nickname :  {item[0].nickname}</p>
+         <p> Portrayed : {item[0].portrayed}</p>
+         <p> Status : {item[0].status}</p>
+       </Col>
+     </Row>
+    </Container>
   )
 }
 
