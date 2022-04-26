a6d54e4748970c3cd567d5288a7111b0f3f663b4
rabia ölker <rabia.olker@gmail.com>
2022-04-26 20:07:40 +0300

    Detail information of the character was taken with axios

--- src/components/detail/detail.js
+++ src/components/detail/detail.js
@@ -1,29 +1,42 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
-import {useSelector} from "react-redux"
 import {Container, Row, Col} from "react-bootstrap"
+import axios from 'axios';
 
 function Detail() {
+  const [char, setChar]=useState(null)
+  const [loading, setLoading]=useState(true)
   let { char_id } = useParams(); // Getting id from URL with useParams
-  char_id=char_id.match(/[0-9]+/gi).join("") //Converting the drawn id to number
-  
-  const items=useSelector(state=>state.character.items)
-  const item=items.filter(item=> item.char_id===Number(char_id)) //Finding the character that matches the id from the URL
- 
+  useEffect( ()=>{
+     axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters/${char_id}`).then(res=>res.data).then(data=>setChar(data)).finally(()=> setLoading(false))
+  },[char_id])
+ console.log(char)
   return (
     <Container>
-     <Row className="justify-content-md-center">
-       <Col  >
-          <img  className="detail-img" alt='characterImage' src={item[0].img} />
-       </Col>
-       <Col >
-         <h5>{item[0].name} </h5>
-         <p > Birthday :  {item[0].birthday}</p>
-         <p> Nickname :  {item[0].nickname}</p>
-         <p> Portrayed : {item[0].portrayed}</p>
-         <p> Status : {item[0].status}</p>
-       </Col>
-     </Row>
+     {
+       loading &&(
+          <div className="spinner-border " role="status" style={{padding:"3%", marginLeft:"45%"}}>
+        <span className="visually-hidden ">Loading...</span>
+      </div>
+       )
+     }
+     {
+       char && (
+        <Row className="justify-content-md-center">
+            <Col  >
+            <img  className="detail-img" alt='characterImage' src={char[0].img} />
+            </Col>
+            <Col >
+              <h5>{char[0].name} </h5>
+              <p > Birthday :  {char[0].birthday}</p>
+              <p> Nickname :  {char[0].nickname}</p>
+              <p> Portrayed : {char[0].portrayed}</p>
+              <p> Status : {char[0].status}</p>
+            </Col>
+        </Row>
+       )
+     }
+    
     </Container>
   )
 }
